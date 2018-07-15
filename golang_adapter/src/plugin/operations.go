6f9c935758e02baa24eab45d66e6b9286a03abd7
DeWayne <dewayne@cloudify.co>
2018-07-15 02:59:14 +0000

    updated operations.go example

--- golang_adapter/src/plugin/operations.go
+++ golang_adapter/src/plugin/operations.go
@@ -5,7 +5,8 @@ import "fmt"
 // Example plugin function
 func func1(ctx_port string, args []string) {
 	fmt.Println("FUNC1 CALLED")
-	call_context(ctx_port, "node", "name")
+	res, err := call_context(ctx_port, "node", "properties", "prop1")
+	fmt.Println("GOT VALUE of PROP1 = " + res)
 	call_context(ctx_port, "logger", "info", "LOGGING SOMETHING FROM PLUGIN")
 }
 
