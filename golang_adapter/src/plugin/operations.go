c9550b5bba712b3318082278ebf1879bcc0c2651
DeWayne <dewayne@cloudify.co>
2018-08-20 18:15:49 +0000

    removed err ref

--- golang_adapter/src/plugin/operations.go
+++ golang_adapter/src/plugin/operations.go
@@ -5,7 +5,7 @@ import "fmt"
 // Example plugin function
 func func1(ctx_port string, args []string) {
 	fmt.Println("FUNC1 CALLED")
-	res, err := call_context(ctx_port, "node", "properties", "prop1")
+	res, _ := call_context(ctx_port, "node", "properties", "prop1")
 	fmt.Printf("GOT VALUE of PROP1 = %s\n", res)
 	call_context(ctx_port, "logger", "info", "LOGGING SOMETHING FROM PLUGIN")
 }
